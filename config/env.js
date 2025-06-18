462df5fe1801c3b5ced76248ffb0e3c1d410667b
matroxok <matrox.biznesyt@gmail.com>
2025-06-18 15:14:28 +0200

    Subscription API Tracker

---
+++ config/env.js
@@ -0,0 +1,6 @@
+import { config } from 'dotenv'
+
+// dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
+config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }) // Zmieniona linia wywołania
+
+export const { PORT, NODE_ENV } = process.env
