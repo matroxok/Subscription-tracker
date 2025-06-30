fdc7849479b7d78fb3033dbaf22cdfcb06be771d
matroxok <matrox.biznesyt@gmail.com>
2025-06-30 22:00:31 +0200

    added: user model

--- models/user.model.js
+++ models/user.model.js
@@ -19,4 +19,18 @@ const userSchema = new mongoose.Schema(definition, {
 		maxLength: 255,
 		match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
 	},
+
+	password: {
+		type: String,
+		requiredq: [true, 'Password is required'],
+		trim: true,
+		minLength: 3,
+		maxLength: 64,
+	},
+	options: {
+		timestamps: true,
+	},
 })
+
+const User = mongoose.model('User', userSchema)
+export default User
